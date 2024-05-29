73ae2e17ba392ce2f4199452512665ad3df562c2
underthetreee <153267269+underthetreee@users.noreply.github.com>
2024-05-29 12:00:33 +0000

    Task 1

--- 1/main.go
+++ 1/main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+type Human struct {
+	Name   string
+	Age    int
+	Gender string
+}
+
+func (h *Human) Say(phrase string) {
+	fmt.Println(phrase)
+}
+
+type Action struct {
+	Human
+}
+
 func main() {
-	fmt.Println("init")
+	person := Action{
+		Human{
+			Name:   "Alex",
+			Age:    25,
+			Gender: "male",
+		},
+	}
+	person.Say("hello")
 }
